b4517c0f76a97bd166ea825c9b1b79ba469245c8
Termanteuse <bomcon123456@gmail.com>
2020-04-04 13:50:39 +0700

    Change Bing Image Fetcher

--- ankiword/utils/bing_image_fetcher.py
+++ ankiword/utils/bing_image_fetcher.py
@@ -3,6 +3,7 @@
 # Greatly thanks to you <3!
 
 import os
+from pathlib import Path
 import urllib.request
 import re
 import threading
@@ -25,43 +26,40 @@ class BingImageFetcher:
         'Gecko/20100101 Firefox/60.0'
     }
 
-    def __init__(self, output_dir='./bing', timeout=5):
-        self.tried_urls = []
-        self.image_md5s = {}
-        self.in_progress = 0
+    def __init__(self, output_dir='../data/imgs', timeout=5):
         self.output_dir = output_dir
         self.timeout = timeout
 
-    def _download(self, pool_sema: threading.Semaphore, url: str):
-        if url in self.tried_urls:
-            return
-
-        pool_sema.acquire()
-        self.in_progress += 1
-
+    def get_filename_from_url(self, url, save_name):
         path = urllib.parse.urlsplit(url).path
         filename = posixpath.basename(path)\
             .split('?')[0]  # Strip GET parameters from filename
         name, ext = os.path.splitext(filename)
         name = name[:36].strip()
+        if not ext:
+            ext = '.jpg'
 
-        filename = name + ext
+        return save_name + ext
 
+    def _download_img_read_md5(self, url, filename):
+        # Get image data buffer
+        request = urllib.request.Request(url, None, self.urlopenheader)
+        image = urllib.request.urlopen(request).read()
+        # Check if the buffer is an image
+        if not imghdr.what(None, image):
+            print('Invalid image, not saving ' + filename)
+            return
+
+        md5_key = hashlib.md5(image).hexdigest()
+        return image, md5_key
+
+    def _download(self, pool_sema: threading.Semaphore,
+                  url: str, save_name: str):
+
+        pool_sema.acquire()
+        filename = self.get_filename_from_url(url, save_name)
         try:
-            # Get image data buffer
-            request = urllib.request.Request(url, None, self.urlopenheader)
-            image = urllib.request.urlopen(request).read()
-            # Check if the buffer is an image
-            if not imghdr.what(None, image):
-                print('Invalid image, not saving ' + filename)
-                return
-
-            # Check duplicate images
-            md5_key = hashlib.md5(image).hexdigest()
-            if md5_key in self.image_md5s:
-                print('Image is a duplicate of ' +
-                      self.image_md5s[md5_key] + ', not saving ' + filename)
-                return
+            image, md5_key = self._download_img_read_md5(url, filename)
 
             i = 0
             # If we see a file in folder has this name:
@@ -77,61 +75,55 @@ class BingImageFetcher:
                 i += 1
                 filename = "%s-%d%s" % (name, i, ext)
 
-            self.image_md5s[md5_key] = filename
-
             imagefile = open(full_path_to_file, 'wb')
             imagefile.write(image)
             imagefile.close()
-            print("OK: " + filename)
-            self.tried_urls.append(url)
+            print("File downloaded: " + filename)
         except Exception:
-            print("FAIL: " + filename)
+            print("Fail to download: " + filename)
         finally:
             pool_sema.release()
-            self.in_progress -= 1
+
+    def _get_bing_url(self, keyword, current, adlt, filters):
+        # Return the url as this example:
+        # https://www.bing.com/images/async?q="apple"&first=10&count=35&adlt=1&qft=''
+        if not filters:
+            filters = ''
+        request_url = self.async_bing_url + \
+            urllib.parse.quote_plus(keyword) + '&first=' + \
+            str(current) + '&count=35&adlt=' + adlt + \
+            '&qft=' + filters
+
+        request = urllib.request.Request(
+            request_url, None, headers=self.urlopenheader)
+        response = urllib.request.urlopen(request)
+
+        # Scrape from HTML links to images
+        html = response.read().decode('utf8')
+        # regex: murl":"(.*?)"
+        links = re.findall('murl&quot;:&quot;(.*?)&quot;', html)
+
+        return links
 
     def _fetch_images_from_keyword(self, pool_sema: threading.Semaphore,
                                    keyword: str, adlt: str, limit: int,
                                    filters=None, out_dir=None):
         current = 0
         last = ''
-
         while True:
-            time.sleep(0.1)
-
-            if self.in_progress > 10:
-                continue
-            if not filters:
-                filters = ''
-
-            # Return the url as this example:
-            # https://www.bing.com/images/async?q="apple"&first=10&count=35&adlt=1&qft=''
-            request_url = self.async_bing_url + \
-                urllib.parse.quote_plus(keyword) + '&first=' + \
-                str(current) + '&count=35&adlt=' + adlt + \
-                '&qft=' + filters
-
-            request = urllib.request.Request(
-                request_url, None, headers=self.urlopenheader)
-            response = urllib.request.urlopen(request)
-
-            # Scrape from HTML links to images
-            html = response.read().decode('utf8')
-            # murl":"(.*?)"
-            links = re.findall('murl&quot;:&quot;(.*?)&quot;', html)
+            links = self._get_bing_url(keyword, current, adlt, filters)
             threads_arr = []
             try:
-                # If we reach the end, finish
+                # In the next loop, if the last link we find is the previous last
+                # We stop the loop
                 if links[-1] == last:
                     return
                 # Go for each links we find by the regex, download
                 for index, link in enumerate(links):
-                    print(index)
                     if limit is not None and current + index >= limit:
                         return threads_arr
                     t = multiprocessing.Process(target=self._download, args=(
-                        pool_sema, link))
-                    # self._download(pool_sema, link)
+                        pool_sema, link, keyword+str(index)))
                     threads_arr.append(t)
                     current += 1
                 last = links[-1]
@@ -140,25 +132,17 @@ class BingImageFetcher:
                 print('No search results for "{0}"'.format(keyword))
                 return
 
-    def _backup_history(self):
-        download_history = open(os.path.join(
-            self.output_dir, 'download_history.pickle'), 'wb')
-        print('Currently saving accessed link to db...')
-        pickle.dump(self.tried_urls, download_history)
-
-        copied_image_md5s = dict(self.image_md5s)
-        pickle.dump(copied_image_md5s, download_history)
-        download_history.close()
-        print('history_dumped')
-
     def download_from_word(self, word, limit=30, threads=5,
-                           adult='', filters=None):
+                           adult=True, filters=None):
+
+        word = word.strip().lower()
 
         if not os.path.exists(self.output_dir):
             os.makedirs(self.output_dir)
         pool_sema = threading.BoundedSemaphore(threads)
 
         print('Starting fetching')
+        adult = '' if adult else 'off'
         th = self._fetch_images_from_keyword(
             pool_sema, word, adult, limit)
 
@@ -171,29 +155,9 @@ class BingImageFetcher:
             if t.is_alive():
                 t.terminate()
 
-    # def download_from_file(self, file_src, limit=30, threads=5,
-    #                        adult='', filters=None):
-    #     try:
-    #         inputFile = open(file_src)
-    #     except (OSError, IOError):
-    #         print("Couldn't open file {}".format(file_src))
-    #         raise Exception
-    #     pool_sema = threading.BoundedSemaphore(threads)
-
-    #     for keyword in inputFile.readlines():
-    #         output_sub_dir = os.path.join(
-    #             self.output_dir, keyword.strip().replace(' ', '_'))
-    #         if not os.path.exists(output_sub_dir):
-    #             os.makedirs(output_sub_dir)
-    #         self._fetch_images_from_keyword(
-    #             pool_sema, keyword,
-    #             adult, limit, out_dir=output_sub_dir)
-
-    #         self._backup_history()
-    #         time.sleep(10)
-    #     inputFile.close()
-
 
 if __name__ == "__main__":
     fetcher = BingImageFetcher()
-    fetcher.download_from_word('Apple')
+    fetcher.download_from_word('kind', limit=50)
+    # fetcher.download_from_word('apple', limit=50)
+    # fetcher.download_from_word('tree', limit=50)
