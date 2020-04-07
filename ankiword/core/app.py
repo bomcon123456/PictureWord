7b42908e322d847ad02cf09eff887ce4d7933f16
Termanteuse <bomcon123456@gmail.com>
2020-04-07 10:23:35 +0700

    More UI.2

--- ankiword/core/app.py
+++ ankiword/core/app.py
@@ -1,3 +1,4 @@
+import sys
 from os import system, name
 import json
 
@@ -12,6 +13,7 @@ def pretty_print_dict(d):
 
 
 class App:
+
     def __init__(self):
         self.args_parser = ArgsParser()
         self.fetcher = BingImageFetcher()
@@ -26,29 +28,23 @@ class App:
         self.current_def_index = 0
         self.current_example_index = 0
         # Pick american pronounciation first.
-        self.current_pronounciation_index = 1
+        self.current_pronunciation_index = 1
         self.image_url = None
 
-        # self.word_dict = {
-        #     "name": None,
-        #     "definitions": [],
-        #     "pronunciations": [],
-        #     "examples": [],
-        #     "image_src": None
-        # }
-
-    @staticmethod
-    def get_int_input(lower, higher):
-        try:
-            n = int(input())
-            if n <= lower or n >= higher:
-                raise
-            n -= 1
-        except:
-            print('[Error] Invalid selection, please try again.')
-            n = -1
-        finally:
-            return n
+    @classmethod
+    def get_int_input(cls, lower, higher):
+        n = -1
+        while True:
+            try:
+                n = int(input())
+                if n < lower or n > higher:
+                    raise
+                n -= 1
+                return n
+            except ValueError:
+                print('[Error] Invalid selection, please try again.')
+            except (EOFError, KeyboardInterrupt):
+                cls.stop()
 
     def clear(self):
         # check and make call for specific operating system
@@ -59,20 +55,31 @@ class App:
         self.set_current_word(word)
         self.print_UI()
 
+    @staticmethod
+    def stop():
+        print('')
+        print('Thank you for using!')
+        sys.exit(0)
+
+    def pick_definition(self):
+        n = self.print_property("def")
+        self.pick_property("def", n)
+        n = self.print_property("exm")
+        self.pick_property("exm", n)
+        self.print_UI()
+
     def get_current_definition(self):
         return self.get_definitions()[self.current_def_index]
 
     def get_current_example(self):
-        return self.get_example_of_definitions()[self.current_example_index]
+        return self.get_examples_of_definitions()[self.current_example_index]
 
     def get_current_pronunciation(self):
-        return self.get_pronunciations()[self.current_pronounciation_index]
+        return self.get_pronunciations()[self.current_pronunciation_index]
 
-    def get_example_of_definitions(self):
+    def get_examples_of_definitions(self):
         entry = self.word_info["definitions"][self.current_def_index]
         definition_dict = entry["definitions"][0]
-        # import json
-        # print(json.dumps(definition_dict, indent=2, sort_keys=True))
         word_example = definition_dict["examples"]
         return word_example
 
@@ -84,42 +91,62 @@ class App:
                 entry))
         return self.definitions
 
-    def print_definitions(self):
+    def print_property(self, prop, pick=False):
+        print_dict_option = {
+            "prn": {
+                "txt": "Pronunciations",
+                "func": self.get_pronunciations,
+                "idx": self.current_pronunciation_index,
+                "cb": self.print_UI},
+            "def": {
+                "txt": "Definitions",
+                "func": self.get_definitions,
+                "idx": self.current_def_index,
+                "cb": self.print_UI},
+
+            "exm": {
+                "txt": "Examples",
+                "func": self.get_examples_of_definitions,
+                "idx": self.current_example_index}
+        }
+
+        current_opt = print_dict_option[prop]
         self.clear()
         print('Your word: "{}"'.format(self.word.capitalize()))
-        print('Definitions:')
-        defs = self.get_definitions()
+        print('{}:'.format(current_opt["txt"]))
+        print()
+        array = current_opt["func"]().copy()
+        array.append("Back")
 
-        for i in range(len(defs)):
-            if i == self.current_def_index:
+        for i in range(len(array)):
+            if i == current_opt["idx"]:
                 print('*', end=' ')
-            print('{0}. {1}'.format(i+1, defs[i]))
+            print('{0}. {1}'.format(i+1, array[i]))
         print('Options[1-{}]: '.format(i+1), end=' ')
 
-    def pick_definitions(self):
-        n = self.get_int_input(0, len(self.get_definitions()))
-        if n == -1:
-            return
+        return i+1
 
-        self.current_def_index = n
-
-    def print_example(self):
-        self.clear()
-        print('Your word: "{}"'.format(self.word.capitalize()))
-        print('Examples:')
-        exs = self.get_example_of_definitions()
-
-        for i in range(len(exs)):
-            if i == self.current_example_index:
-                print('*', end=' ')
-            print('{0}. {1}'.format(i+1, exs[i]))
-        print('Options[1-{}]: '.format(i+1), end=' ')
-
-    def pick_example(self):
-        n = self.get_int_input(0, len(self.get_example_of_definitions()))
-        if n == -1:
-            return
-        self.current_example_index = n
+    def pick_property(self, prop, back_index):
+        """
+            Return False when user want to go back to previous UI
+        """
+        callbacks = {
+            "exm": self.pick_definition,
+            "def": self.print_UI,
+            "prn": self.print_UI
+        }
+        n = self.get_int_input(1, back_index)
+        # n is zero-based
+        if n == back_index-1:
+            callbacks[prop]()
+        elif prop == "def":
+            self.current_def_index = n
+            # Set to the first one just in case
+            self.current_example_index = 0
+        elif prop == "prn":
+            self.current_pronunciation_index = n
+        elif prop == "exm":
+            self.current_example_index = n
 
     def get_pronunciations(self):
         return self.word_info["pronunciations"]
@@ -141,7 +168,7 @@ class App:
         if pronunciation['ipa']:
             print('IPA:', pronunciation['ipa'], end=' --- ')
             print('Current audio file is',
-                  'NAmE.' if self.current_pronounciation_index else 'BrE.')
+                  'NAmE.' if self.current_pronunciation_index else 'BrE.')
         if self.image_url:
             print("Picture word: Loaded.")
         else:
@@ -154,20 +181,31 @@ class App:
         print(
             'There are {0} other definitions and {1} other pronunciation speaker.'.format(
                 str(def_length-1), str(pro_length-1)
-            ))
-        print('1. See other definitions')
-        print('2. Change pronunciation speaker')
-        print('3. Choose picture word')
-        print('3. Save to Anki')
-        print('4. Exit')
+            ), end='\n\n')
+
+        options = [("sav", "Save to Anki"), ("ext", "Exit")]
+        if pro_length > 1:
+            options.insert(0, ("prn", "Change pronunciation speaker"))
+        if def_length > 1:
+            options.insert(0, ("def", "See other definitions"))
+
+        for i in range(len(options)):
+            print("{0}. {1}".format(i+1, options[i][1]))
         print('Option [1-4]: ', end='')
-        n = int(input())
-        if n == 1:
-            self.print_definitions()
-            self.pick_definitions()
-            self.print_example()
-            self.pick_example()
+
+        n = self.get_int_input(1, 4)
+        action = options[n][0]
+        if action == "def":
+            n = self.print_property("def")
+            self.pick_property("def", n)
+            n = self.print_property("exm")
+            self.pick_property("exm", n)
             self.print_UI()
+        elif action == "prn":
+            self.print_property("prn")
+            self.pick_pronun()
+        elif action == "ext":
+            self.stop()
 
 
 if __name__ == "__main__":
